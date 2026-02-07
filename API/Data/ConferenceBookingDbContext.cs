de4cd0c78274537e11c30a3fed5f8a52f470748a
Onthangaho <onthangahomagoro@gmail.com>
2026-02-08 01:52:40 +0200

    refactor: remove unnecessary namespace declaration in ConferenceBookingDbContext

--- API/Data/ConferenceBookingDbContext.cs
+++ API/Data/ConferenceBookingDbContext.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data
-{   
+
 public class ConferenceBookingDbContext : IdentityDbContext<ApplicationUser>
 {
     public ConferenceBookingDbContext(DbContextOptions<ConferenceBookingDbContext> options) : base(options)
     {
     }
 
-}
 }
