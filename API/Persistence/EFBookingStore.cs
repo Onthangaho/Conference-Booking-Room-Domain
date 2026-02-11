22b1ac719f35c44ca58ab1560e0354df510e2449
Onthangaho <onthangahomagoro@gmail.com>
2026-02-12 00:38:37 +0200

    feat: refactor EFBookingStore methods to include room attachment and improve booking retrieval

--- API/Persistence/EFBookingStore.cs
+++ API/Persistence/EFBookingStore.cs
@@ -11,12 +11,31 @@ public class EFBookingStore : IBookingStore
 
     public async Task SaveAsync(Booking booking)
     {
+
+        _dbContext.ConferenceRooms.Attach(booking.Room); // Attach the room to the context to avoid duplicate entries
         _dbContext.Bookings.Add(booking);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Booking booking)
+    {
+        _dbContext.ConferenceRooms.Attach(booking.Room); // Attach the room to the context to avoid issues with foreign key constraints
+        _dbContext.Bookings.Update(booking);
         await _dbContext.SaveChangesAsync();
     }
 
     public async Task<List<Booking>> LoadBookingAsync()
     {
-        return await _dbContext.Bookings.OrderByDescending(b => b.Start).ToListAsync();
+        return await _dbContext.Bookings
+        .Include(b => b.Room) // Include the related ConferenceRoom entity
+        .OrderByDescending(b => b.Start)
+        .ToListAsync();
+    }
+
+    public async Task DeleteAsync(Booking booking)
+    {
+        _dbContext.ConferenceRooms.Attach(booking.Room); // Attach the room to the context to avoid issues with foreign key constraints
+        _dbContext.Bookings.Remove(booking);
+        await _dbContext.SaveChangesAsync();
     }
 }
