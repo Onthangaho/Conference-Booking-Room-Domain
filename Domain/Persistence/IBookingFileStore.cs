3c0e0e1a0e93d08042f7ffecef1b7cc549934a5e
Onthangaho <onthangahomagoro@gmail.com>
2026-02-12 00:38:22 +0200

    feat: add UpdateAsync and DeleteAsync methods to IBookingStore interface

--- Domain/Persistence/IBookingFileStore.cs
+++ Domain/Persistence/IBookingFileStore.cs
@@ -3,4 +3,6 @@ public interface IBookingStore
 {
     Task SaveAsync(Booking booking);
     Task<List<Booking>> LoadBookingAsync();
+    Task UpdateAsync(Booking booking);
+    Task DeleteAsync(Booking booking);
 }
