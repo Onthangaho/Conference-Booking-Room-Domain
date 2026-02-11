4a5d50fb6584ab1316bffab96d57ddcc97ab6d2b
Onthangaho <onthangahomagoro@gmail.com>
2026-02-11 08:37:19 +0200

    feat: add location and isActive values to conference room seed data

--- API/Data/ConferenceRoomSeeder.cs
+++ API/Data/ConferenceRoomSeeder.cs
@@ -2,26 +2,26 @@ using Microsoft.EntityFrameworkCore;
 
 public static class ConferenceRoomSeeder
 {
-    public static async Task SeedRooms(ConferenceBookingDbContext context)
+    public static async Task SeedRoomsAsync(ConferenceBookingDbContext context)
     {
         var rooms = new List<ConferenceRoom>
         {
-            new ConferenceRoom { Name = "Room A", Capacity = 10, RoomType = RoomType.Standard },
-                new ConferenceRoom { Name = "Room B", Capacity = 20, RoomType = RoomType.BoardRoom },
-                new ConferenceRoom { Name = "Room C", Capacity = 15, RoomType = RoomType.Training },
-                new ConferenceRoom { Name = "Room D", Capacity = 25, RoomType = RoomType.Standard },
-                new ConferenceRoom { Name = "Room E", Capacity = 30, RoomType = RoomType.BoardRoom },
-                new ConferenceRoom { Name = "Room F", Capacity = 10, RoomType = RoomType.Training },
-                new ConferenceRoom { Name = "Room G", Capacity = 20, RoomType = RoomType.Standard },
-                new ConferenceRoom { Name = "Room H", Capacity = 15, RoomType = RoomType.BoardRoom },
-                new ConferenceRoom { Name = "Room I", Capacity = 13, RoomType = RoomType.Training },
-                new ConferenceRoom { Name = "Room J", Capacity = 20, RoomType = RoomType.Standard },
-                new ConferenceRoom { Name = "Room K", Capacity = 10, RoomType = RoomType.BoardRoom },
-                new ConferenceRoom { Name = "Room L", Capacity = 5, RoomType = RoomType.Training },
-                new ConferenceRoom { Name = "Room M", Capacity = 12, RoomType = RoomType.Standard },
-                new ConferenceRoom { Name = "Room N", Capacity = 15, RoomType = RoomType.BoardRoom },
-                new ConferenceRoom { Name = "Room O", Capacity = 12, RoomType = RoomType.Training },
-                new ConferenceRoom { Name = "Room P", Capacity = 30, RoomType = RoomType.Standard }
+            new ConferenceRoom { Name = "Room A", Capacity = 10, RoomType = RoomType.Standard , Location = "Cape Town", IsActive = true},
+                new ConferenceRoom { Name = "Room B", Capacity = 20, RoomType = RoomType.BoardRoom, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room C", Capacity = 15, RoomType = RoomType.Training, Location = "Cape Town", IsActive = false },
+                new ConferenceRoom { Name = "Room D", Capacity = 25, RoomType = RoomType.Standard, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room E", Capacity = 30, RoomType = RoomType.BoardRoom, Location = "Cape Town", IsActive = false },
+                new ConferenceRoom { Name = "Room F", Capacity = 10, RoomType = RoomType.Training, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room G", Capacity = 20, RoomType = RoomType.Standard, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room H", Capacity = 15, RoomType = RoomType.BoardRoom, Location = "Cape Town", IsActive = true },
+                new ConferenceRoom { Name = "Room I", Capacity = 13, RoomType = RoomType.Training, Location = "Cape Town", IsActive = true },
+                new ConferenceRoom { Name = "Room J", Capacity = 20, RoomType = RoomType.Standard, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room K", Capacity = 10, RoomType = RoomType.BoardRoom, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room L", Capacity = 5, RoomType = RoomType.Training, Location = "Cape Town", IsActive = true },
+                new ConferenceRoom { Name = "Room M", Capacity = 12, RoomType = RoomType.Standard, Location = "Bloemfontein", IsActive = true },
+                new ConferenceRoom { Name = "Room N", Capacity = 15, RoomType = RoomType.BoardRoom, Location = "Cape Town", IsActive = false },
+                new ConferenceRoom { Name = "Room O", Capacity = 12, RoomType = RoomType.Training, Location = "Cape Town", IsActive = true },
+                new ConferenceRoom { Name = "Room P", Capacity = 30, RoomType = RoomType.Standard, Location = "Cape Town", IsActive = true }
             };
 // Check if the rooms already exist in the database to avoid duplicates
             foreach (var room in rooms)
