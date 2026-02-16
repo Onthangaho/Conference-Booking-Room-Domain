41576d57882723e654e587adc664cf0fcd48216a
Onthangaho <onthangahomagoro@gmail.com>
2026-02-16 23:04:24 +0200

    feat: add Navbar component for navigation in the application

---
+++ CLIENT/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+
+
+    return (
+        <nav className="navbar">
+            <h1>Conference Room Booking</h1>
+
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Bookings</a>
+                <a href="#">Rooms</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
