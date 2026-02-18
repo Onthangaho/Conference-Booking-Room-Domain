f9085a4dfaebcac73a15f601660921561ce43171
Onthangaho <onthangahomagoro@gmail.com>
2026-02-18 09:59:18 +0200

    feat: add type prop to Button component for improved flexibility

--- CLIENT/src/components/Button.jsx
+++ CLIENT/src/components/Button.jsx
@@ -1,12 +1,15 @@
 //this is a reusable button component that can be used throughout the application. It accepts a label, an onClick handler, and a variant for styling.
 
-function Button({label, onClick, variant = "primary"}) {
-
-    return (
-        <button className={`btn ${variant}`} onClick={onClick}>
-            {label}
-            </button>
-    );
+function Button({ label, onClick, variant = "primary", type = "button" }) {
+  return (
+    <button 
+      type={type} 
+      className={`btn ${variant}`} 
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 }
 
 export default Button;
