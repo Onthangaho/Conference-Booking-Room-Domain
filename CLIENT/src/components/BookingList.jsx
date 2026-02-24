f2734908f769df5293a689f41c1b9ded7fcb69f8
Onthangaho <onthangahomagoro@gmail.com>
2026-02-24 03:12:11 +0200

    refactor: clean up BookingList component for improved readability

--- CLIENT/src/components/BookingList.jsx
+++ CLIENT/src/components/BookingList.jsx
@@ -1,17 +1,13 @@
-
 import BookingCard from "./BookingCard";
 
-
-
 function BookingList({ bookings, deleteBooking }) {
-
-    return(
-        <div className="bookings-list">
-            {bookings.map((booking) => (
-                <BookingCard key={booking.id} booking={booking} deleteBooking={deleteBooking}/> 
-            ))}
-        </div>
-    )
+  return (
+    <div className="bookings-list">
+      {bookings.map((booking) => (
+        <BookingCard key={booking.id} booking={booking} deleteBooking={deleteBooking} />
+      ))}
+    </div>
+  );
 }
 
 export default BookingList;
