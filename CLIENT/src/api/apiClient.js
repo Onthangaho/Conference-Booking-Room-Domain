22b20a42a7f02151d6b0e1750c64237dc73fdbf7
Onthangaho <onthangahomagoro@gmail.com>
2026-03-09 22:27:53 +0200

    refactor: improve formatting and comments in apiClient.js

--- CLIENT/src/api/apiClient.js
+++ CLIENT/src/api/apiClient.js
@@ -1,36 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 5000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-//request interceptor
+// Request interceptor
 apiClient.interceptors.request.use(
-    (config) => {
-
-        console.log(`Sending ${config.method?.toUpperCase()} request to ${config.url} with data: `, config.data);
-        const token = localStorage.getItem('token');
-        // If token exists, add it to the Authorization header
-        if (token)
-            config.headers.Authorization = `Bearer ${token}`;
-        return config;
-    }, 
-    // Handle request errors when they occur because of network issues or other reasons
-    (error) => {
-        console.error('Error in request interceptor: ', error);
-        return Promise.reject(error);
+  (config) => {
+    console.log(
+      `Sending ${config.method?.toUpperCase()} request to ${config.url} with data:`,
+      config.data
+    );
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    console.error("Error in request interceptor:", error);
+    return Promise.reject(error);
+  }
 );
 
-//Response interceptor
-// This will handle the response and extract the data, and also catch any errors that occur during the response phase, such as server errors or invalid responses.
+// Response interceptor
 apiClient.interceptors.response.use(
   (response) => response.data,
   (error) => {
+    // Ignore cancellation errors
+    if (axios.isCancel(error) || error?.code === "ERR_CANCELED") {
+      console.log("Request cancelled by AbortController");
+      return Promise.reject(error);
+    }
+
+    // Handle unauthenticated requests globally
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("role");
+      localStorage.removeItem("username");
+      window.location.href = "/";
+    }
+
     console.error("API Error:", error.message);
     return Promise.reject(error);
   }
