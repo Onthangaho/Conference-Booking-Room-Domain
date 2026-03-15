06639d5732be5828555b7b7237998096cfd5377e
Onthangaho <onthangahomagoro@gmail.com>
2026-03-15 14:44:54 +0200

    final commit for next/assignment_3.3

--- booking-next/src/hooks/useDebouncedValue.ts
+++ booking-next/src/hooks/useDebouncedValue.ts
@@ -1,5 +1,5 @@
 "use client";
-
+// This hook is used to debounce a value, which means that it will only update the debounced value after a certain amount of time has passed since the last change. This is useful for performance optimization, especially when dealing with user input or API calls that should not be triggered on every keystroke.
 import { useEffect, useState } from "react";
 
 export function useDebouncedValue<T>(value: T, delayMs = 400): T {
